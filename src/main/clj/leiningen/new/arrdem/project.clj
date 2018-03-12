1e58518f561decc891afcde4321bfa15a1b76d63
Reid 'arrdem' McKenzie <me@arrdem.com>
2018-03-11 20:48:00 -0700

    Add a my lein-auto and cljfmt as deps, tweak their configuration

--- src/main/clj/leiningen/new/arrdem/project.clj
+++ src/main/clj/leiningen/new/arrdem/project.clj
@@ -21,17 +21,31 @@
    {:source-paths      ["src/dev/clj"
                         "src/dev/cljc"]
     :java-source-paths ["src/dev/jvm"]
-    :resource-paths    ["src/dev/resources"]}}
+    :resource-paths    ["src/dev/resources"]
+    :doc-paths         ["README.md" "docs"]}}
 
-  :plugins [[me.arrdem/lein-git-version "2.0.5"]]
+  :plugins [[me.arrdem/lein-git-version "2.0.5"]
+            [me.arrdem/lein-auto "0.1.4"]
+            [lein-cljfmt "0.5.7"]]
 
   :git-version
   {:status-to-version
-   (fn [{:keys [tag version ahead ahead? dirty?] :as git}]
+   (fn [{:keys [tag version branch ahead ahead? dirty?] :as git}]
      (if (and tag (not ahead?) (not dirty?))
-       tag
-       (if tag
-         (str tag
-              (when ahead? (str "." ahead))
-              (when dirty? "-SNAPSHOT"))
-         "0.1.0-SNAPSHOT")))})
+       (do (assert (re-find #"\d+\.\d+\.\d+" tag)
+                   "Tag is assumed to be a raw SemVer version")
+           tag)
+       (if (and tag (or ahead? dirty?))
+         (let [[_ prefix patch] (re-find #"(\d+\.\d+)\.(\d+)" tag)
+               patch            (Long/parseLong patch)
+               patch+           (inc patch)]
+           (format "%s.%d-%s-SNAPSHOT" prefix patch+ branch))
+         "0.1.0-SNAPSHOT")))}
+
+  :auto
+  {:default {:file-pattern #"\.(clj|cljs|cljx|cljc|edn|md)$"
+             :paths        [:java-source-paths
+                            :source-paths
+                            :resource-paths
+                            :test-paths
+                            :doc-paths]}})
